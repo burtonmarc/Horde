cf5911a4d3e7af6f66949d467771768326c4fbd5
MarcVillegas <marcvillegas90@gmail.com>
2022-10-01 20:40:21 +0200

    Added Addressables package

---
+++ Horde/Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour
+{
+    
+    
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
